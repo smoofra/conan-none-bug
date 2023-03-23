ff14ec7240cb91ebf4ab7928038eb2a4baf6f39c
Lawrence D'Anna <larry@elder-gods.org>
2023-03-23 12:20:52 -0400

    found a workaround

--- gcc/conanfile.py
+++ gcc/conanfile.py
@@ -32,21 +32,42 @@ class GccConan(ConanFile):
     description = "GCC, the GNU Compiler Collection"
     no_copy_source = True
 
+    @property
+    def settings_for_target(self):
+        if settings_target := getattr(self, "settings_target", None):
+            return settings_target
+        # It seems when gcc is tool_required from another package building in the
+        # build context, settings_target is None.   By definition in this case what's
+        # need is a compiler for the build machine, so host, build and target
+        # platforms are all the same.  Therefore, if settings_target is not
+        # available, default to building a native compiler for host platform
+        #
+        # see: https://github.com/conan-io/conan/issues/13515
+        return self.settings
+
+    @property
+    def arch_target(self):
+        return self.options.arch_target or self.settings_for_target.arch
+
+    @property
+    def os_target(self):
+        return self.options.os_target or self.settings_for_target.os
+
+    def validate(self):
+        if not self.options.os_target or not self.options.arch_target:
+            raise Exception("oh no")
 
     def configure(self):
-        # record target information in options
-        if settings_target := getattr(self, "settings_target", None):
-            self.options.os_target = self.options.os_target or settings_target.os
-            self.options.arch_target = self.options.arch_target or settings_target.arch
-
+        self.options.arch_target = self.arch_target
+        self.options.os_target = self.os_target
 
     def requirements(self):
         self.requires(
             'binutils/2.38@bug',
             visible=False,
             options={
-                "arch_target": self.options.arch_target,
-                "os_target": self.options.os_target,
+                "arch_target": self.arch_target,
+                "os_target": self.os_target,
             },
         )
 
